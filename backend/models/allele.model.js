0fcf24328730af35caca056dbecad56d5b1bb8b3
jhwna <hanwoolna@gmail.com>
2024-10-29 22:21:19 -0400

    Updates to models
    - Added models for Drug, Clinical Annotation
    - Implemented methods for Test Result model
    -

--- backend/models/allele.model.js
+++ backend/models/allele.model.js
@@ -1,19 +1,43 @@
 const mongoose = require('mongoose');
 
-// ENUM
+// ENUMS
 const AlleleFunction = Object.freeze({
-    Normal: Symbol('NORMAL_FUNCTION'),
-    Increased: Symbol('INCREASED_FUNCTION'),
-    Decreased: Symbol('DECREASED_FUNCTION'),
-    None: Symbol('NO_FUNCTION')
+    INCREASED: 3,
+    NORMAL: 2,
+    DECREASED: 1,
+    NONE: 0
 });
-module.exports = AlleleFunction;
+module.exports.AlleleFunction = AlleleFunction;
 
+const AllelePhenotype = Object.freeze({
+    ULTRARAPID: 4,
+    RAPID: 3,
+    NORMAL: 2,
+    INTERMEDIATE: 1,
+    POOR: 0
+});
+module.exports.AllelePhenotype = AllelePhenotype;
+
+// SCHEMA
 const alleleSchema = new mongoose.Schema({
-    alleleName: { type: mongoose.Schema.Types.String, required: true, unique: true },
-    numCopies: { type: mongoose.Schema.Types.Number, required: true, default: 1 },
-    parentGene: { type: mongoose.Schema.Types.ObjectId, ref: 'Gene', required: true },
-    alleleFunction: { type: mongoose.Schema.Types.String, required: true }
+    alleleName: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+    },
+    numCopies: {
+        type: mongoose.Schema.Types.Number,
+        required: true,
+        default: 1
+    },
+    parentGene: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Gene',
+        required: true
+    },
+    alleleFunction: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    }
 });
 
 alleleSchema.methods.getDisplayName = function() {
@@ -24,4 +48,4 @@ alleleSchema.methods.getDisplayName = function() {
 };
 
 const Allele = mongoose.model('Allele', alleleSchema);
-module.exports = Allele;
+module.exports.Allele = Allele;
--- backend/models/gene.model.js
+++ backend/models/gene.model.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
 const geneSchema = new mongoose.Schema({
-    geneName: { type: mongoose.Schema.Types.String, required: true, unique: true },
-    altNames: [{ type: mongoose.Schema.Types.String }], // Alternate names for gene; used when querying test results by gene name
-    description: { type: mongoose.Schema.Types.String },
-    namedAlleles: [{ type: mongoose.Schema.Types.String, ref: 'Allele' }]
+    geneName: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+        unique: true
+    },
+    // Alternate names for gene; used when querying test results by gene name
+    altNames: [{
+        type: mongoose.Schema.Types.String
+    }],
+    description: {
+        type: mongoose.Schema.Types.String
+    },
+    namedAlleles: [{
+        type: mongoose.Schema.Types.String,
+        ref: 'Allele'
+    }]
 });
 
 const Gene = mongoose.model('Gene', geneSchema);
