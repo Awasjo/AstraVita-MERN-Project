73bf2f9e66f9c6afd92d71e616e200d899eca650
Awasjo <awasjomail@gmail.com>
2024-11-01 19:34:56 -0400

    Updated permission logic to accept requests from any user

--- backend/models/doctor.model.js
+++ backend/models/doctor.model.js
@@ -4,30 +4,8 @@ const User = require('./user.model');
 const doctorSchema = new mongoose.Schema({
   jobTitle: String,
   employer: String,
-  approvedPatients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patient' }],
-  permissionRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patient' }]
+  approvedPatients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Patient' }]
 });
-
-doctorSchema.methods.approvePatient = function(patientId) {
-  if (!this.approvedPatients.includes(patientId)) {
-    this.approvedPatients.push(patientId);
-  }
-};
-
-doctorSchema.methods.requestPatientPermission = function(patientId) {
-  if (!this.permissionRequests.includes(patientId) && !this.approvedPatients.includes(patientId)) {
-    this.permissionRequests.push(patientId);
-  }
-};
-
-doctorSchema.methods.handlePatientResponse = function(patientId, approved) {
-  // Remove the patient from requests array
-  this.permissionRequests = this.permissionRequests.filter(id => id.toString() !== patientId.toString());
-  if (approved && !this.approvedPatients.includes(patientId)) {
-    this.approvedPatients.push(patientId);
-  }
-  // rejection just removes the patient from requests array (no logic implemented)
-};
 
 const Doctor = User.discriminator('Doctor', doctorSchema);
 
