42357062c2cd924e07ee95195c212bc3796b156c
Awasjo <awasjomail@gmail.com>
2024-11-19 22:25:47 -0500

    removed comments and cleaned code using prettier extension

--- frontend/src/components/PatientPortal.jsx
+++ frontend/src/components/PatientPortal.jsx
@@ -1,67 +1,73 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-import axios from 'axios';
-import './PatientPortal.css';
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import axios from "axios";
+import "./PatientPortal.css";
 
-const PatientPortal = (props) => {
-    const location = useLocation();
-    const patient = location.state.patient;
-    const [expandedResults, setExpandedResults] = useState({});
-    const [testResults, setTestResults] = useState([]);
-    const fileInputRef = useRef(null);
+const PatientPortal = () => {
+  const location = useLocation();
+  const patient = location.state.patient;
+  const [expandedResults, setExpandedResults] = useState({});
+  const [testResults, setTestResults] = useState([]);
+  const fileInputRef = useRef(null);
 
-    const fetchTestResults = async () => {
-      try {
-          const response = await axios.get(`http://localhost:3000/api/test-results/patient/${patient.id}`, { withCredentials: true });
-          setTestResults(response.data);
-          console.log('testResults:', response.data);
-      } catch (error) {
-          console.error('Error fetching test results:', error);
-      }
+  useEffect(() => {
+    fetchTestResults();
+  }, [patient.id]);
+
+  const fetchTestResults = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/test-results/patient/${patient.id}`,
+        { withCredentials: true }
+      );
+      setTestResults(response.data);
+      console.log("testResults:", response.data);
+    } catch (error) {
+      console.error("Error fetching test results:", error);
+    }
   };
-    useEffect(() => {
-        fetchTestResults();
-    }, [patient.id]);
 
-    const toggleExpand = (id) => {
-        setExpandedResults(prevState => ({
-            ...prevState,
-            [id]: !prevState[id]
-        }));
-    };
+  const toggleExpand = (id) => {
+    setExpandedResults((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
 
-    const handleFileChange = (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = async (e) => {
-          try {
-            const fileContents = e.target.result;
-            const jsonData = JSON.parse(fileContents);
-  
-            // Send the parsed JSON data to the backend
-            const response = await axios.post('http://localhost:3000/api/test-results', jsonData, {
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        try {
+          const fileContents = e.target.result;
+          const jsonData = JSON.parse(fileContents);
+
+          const response = await axios.post(
+            "http://localhost:3000/api/test-results",
+            jsonData,
+            {
               headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
               },
               withCredentials: true,
-            });
-  
-            console.log('Data uploaded successfully:', response.data);
-            // Optionally, fetch the updated test results
-            fetchTestResults();
-          } catch (error) {
-            console.error('Error uploading data:', error);
-          }
-        };
-        reader.readAsText(file);
-      }
-    };
-  
-    const handleUploadTestResult = () => {
-      fileInputRef.current.click();
-    };
+            }
+          );
+
+          console.log("Data uploaded successfully:", response.data);
+          fetchTestResults();
+        } catch (error) {
+          console.error("Error uploading data:", error);
+        }
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  const handleUploadTestResult = () => {
+    fileInputRef.current.click();
+  };
 
   return (
     <div className="patientPortal-container">
@@ -71,7 +77,13 @@ const PatientPortal = (props) => {
       </Helmet>
       <div className="home-desktop-patient-portal-test-results">
         {testResults.map((result) => (
-          <div key={result._id} className={`home-test-result1 ${expandedResults[result._id] ? 'expanded' : ''}`} onClick={() => toggleExpand(result._id)}>
+          <div
+            key={result._id}
+            className={`home-test-result1 ${
+              expandedResults[result._id] ? "expanded" : ""
+            }`}
+            onClick={() => toggleExpand(result._id)}
+          >
             <div className="test-header">
               <div className="home-phenotype">
                 <span className="patientPortal-text21">{result.phenotype}</span>
@@ -82,21 +94,29 @@ const PatientPortal = (props) => {
                 <span className="patientPortal-text24">Diplotype</span>
               </div>
               <div className="home-tested-gene">
-                <span className="patientPortal-text25">{result.testedGene.geneName}</span>
+                <span className="patientPortal-text25">
+                  {result.testedGene.geneName}
+                </span>
                 <span className="patientPortal-text26">Tested Gene</span>
               </div>
               <div className="home-test-date">
-                <span className="patientPortal-text14">{new Date(result.testDate).toLocaleDateString()}</span>
+                <span className="patientPortal-text14">
+                  {new Date(result.testDate).toLocaleDateString()}
+                </span>
                 <span className="patientPortal-text15">Test Date</span>
               </div>
             </div>
             <div className="test-content">
               <div className="home-uploadedby">
-                <span className="patientPortal-text10">{result.uploadedBy.firstName} {result.uploadedBy.lastName}</span>
+                <span className="patientPortal-text10">
+                  {result.uploadedBy.firstName} {result.uploadedBy.lastName}
+                </span>
                 <span className="home-text11">Uploaded by</span>
               </div>
               <div className="home-upload-date">
-                <span className="patientPortal-text12">{new Date(result.uploadDate).toLocaleDateString()}</span>
+                <span className="patientPortal-text12">
+                  {new Date(result.uploadDate).toLocaleDateString()}
+                </span>
                 <span className="home-text13">Upload Date</span>
               </div>
               <img
@@ -104,39 +124,23 @@ const PatientPortal = (props) => {
                 alt="Divider1973"
                 className="home-divider"
               />
-              {/* <div className="home-codeine">
-                <span className="home-text16">
-                  {result.testedGene.geneName} {result.maternalAlleleDisplayName}/{result.paternalAlleleDisplayName} is associated with {result.phenotype} metabolism of codeine.
-                </span>
-                <span className="patientPortal-text17">Codeine</span>
-              </div> */}
-                {result.affectedMedications.map((annotation, index) => (
-                  <div key={index} className="home-debrisoquine">
-                    <span className="home-text18">
-                      {annotation.description}
-                    </span>
-                    {annotation.associatedDrug && (
-                      <>
-                        {/* <span className="patientPortal-text19">
-                          {annotation.associatedDrug.drugName} 
-                        </span> */}
-                        <span className="patientPortal-text19">
-                          {annotation.associatedDrug.description} 
-                        </span>
-                      </>
-                    )}
-                  </div>
-                ))}
-              {/* <div className="home-debrisoquine">
-                <span className="home-text18">
-                  {result.testedGene.geneName} {result.maternalAlleleDisplayName}/{result.paternalAlleleDisplayName} is associated with {result.phenotype} metabolism of debrisoquine.
-                </span>
-                <span className="patientPortal-text19">Debrisoquine</span>
-              </div> */}
+              {result.affectedMedications.map((annotation, index) => (
+                <div key={index} className="home-debrisoquine">
+                  <span className="home-text18">{annotation.description}</span>
+                  {annotation.associatedDrug && (
+                    <>
+                      <span className="patientPortal-text19">
+                        {annotation.associatedDrug.drugName}
+                      </span>
+                      <span className="patientPortal-text19">
+                        {annotation.associatedDrug.description}
+                      </span>
+                    </>
+                  )}
+                </div>
+              ))}
               <span className="home-text20">Affected Medications</span>
-              <div className="home-affected-medications">
-
-              </div>
+              <div className="home-affected-medications"></div>
             </div>
           </div>
         ))}
@@ -149,8 +153,10 @@ const PatientPortal = (props) => {
           <span className="home-text27">Sort by Test Date</span>
         </div>
         <div className="home-search-bar">
-          <input className="patientPortal-text28" placeholder='Filter test results by gene or medication'>
-          </input>
+          <input
+            className="patientPortal-text28"
+            placeholder="Filter test results by gene or medication"
+          />
           <img
             src="../public/external/iconmonstrmagnifier212081-8lkk.svg"
             alt="iconmonstrmagnifier212081"
@@ -161,7 +167,7 @@ const PatientPortal = (props) => {
           type="file"
           accept=".json"
           ref={fileInputRef}
-          style={{ display: 'none' }}
+          style={{ display: "none" }}
           onChange={handleFileChange}
         />
         <button className="home-upload-button" onClick={handleUploadTestResult}>
@@ -172,10 +178,12 @@ const PatientPortal = (props) => {
             className="home-iconmonstrupload181"
           />
         </button>
-        <span className="patientPortal-text30">{patient.username}'s Results</span>
+        <span className="patientPortal-text30">
+          {patient.username}'s Results
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PatientPortal;
