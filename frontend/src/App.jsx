b39b1e32b5e2ea77e68428deaf719bc4459ead13
Stefan Lovecchio <slovecch@my.centennialcollege.ca>
2024-12-02 16:08:44 -0500

    Made homepage CSS responsive, changed probitDevice img to png

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,91 +23,39 @@ const Home = () => {
         <meta property="og:title" content="OneDrug homepage" />
       </Helmet>
       <div className="home-desktop-home">
-        <div className="home-footer">
-          <span className="home-text10">
-            <span>© 2024 OneDrug Inc. All rights reserved.</span>
+        
+        <div className="home-frame1">
+          <div className="home-hero-image">
+          <img
+              alt="Blue2513"
+              src="/external/blue2513-8gd-800w.png"
+              className="home-blue"
+            />
+            <img
+              alt="Gray2512"
+              src="/external/gray2512-91s-800w.png"
+              className="home-gray"
+            />
+            
+            <img
+              alt="Nurse2510"
+              src="/external/nurse2510-5u3o-400w.png"
+              className="home-nurse"
+            />
+          </div>
+          <div className="home-findout-button">
+            <span className="home-text42">
+              <span>Find out how</span>
+            </span>
+          </div>
+          <span className="home-text44">
+            <span>
+              We’re enabling worldwide access to personalized precision
+              medicine.
+            </span>
           </span>
-          <div className="home-social-media">
-            <img
-              alt="iconmonstrtwitter316511"
-              src="/external/iconmonstrtwitter316511-hifv.svg"
-              className="home-iconmonstrtwitter31"
-            />
-            <img
-              alt="iconmonstrlinkedin316511"
-              src="/external/iconmonstrlinkedin316511-0rq8.svg"
-              className="home-iconmonstrlinkedin31"
-            />
-            <img
-              alt="iconmonstrinstagram1316511"
-              src="/external/iconmonstrinstagram1316511-30hf.svg"
-              className="home-iconmonstrinstagram131"
-            />
-            <img
-              alt="iconmonstrfacebook316511"
-              src="/external/iconmonstrfacebook316511-6s1s.svg"
-              className="home-iconmonstrfacebook31"
-            />
-          </div>
         </div>
-        <div className="home-frame4">
-          <div className="home-partners">
-            <span className="home-text12">
-              <span>Trusted by leading institutions worldwide</span>
-            </span>
-            <img
-              alt="GreySloan1063"
-              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/1c8dc7fb-084c-4576-ac70-b5772815c26a?org_if_sml=15119&amp;force_format=original"
-              className="home-grey-sloan"
-            />
-            <img
-              alt="PrincetonPlainsboro1062"
-              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/f2ed2315-43c9-45dd-9ba6-652d699892c3?org_if_sml=111680&amp;force_format=original"
-              className="home-princeton-plainsboro"
-            />
-            <img
-              alt="UHN1062"
-              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/8947b1d9-4a27-422b-b9c9-a69a48e3233e?org_if_sml=13690&amp;force_format=original"
-              className="home-uhn"
-            />
-            <img
-              alt="UofT1062"
-              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/db610096-cc1a-4c16-a6a4-d7fe86039a7e?org_if_sml=17907&amp;force_format=original"
-              className="home-uof-t"
-            />
-          </div>
-        </div>
-        <div className="home-frame3">
-          <div className="home-text14">
-            <div className="home-requestdemo-button">
-              <span className="home-text15">
-                <span>Request a demo</span>
-              </span>
-            </div>
-            <div className="home-learnmore-button">
-              <span className="home-text17">
-                <span>Learn more</span>
-              </span>
-            </div>
-            <span className="home-text19">
-              <span>
-                Our point-of-care pharmacogenomic test device helps doctors
-                personalize patient treatment plans by identifying adverse drug
-                reactions in 20 minutes.
-              </span>
-            </span>
-            <span className="home-text21">ProbeiT</span>
-              <span className="home-text22">OneDrug</span>
-              
-          </div>
-          <div className="home-placeholder-image">
-            <img
-              alt="Rectangle3516"
-              src="/external/probitDevice.JPG"
-              className="home-rectangle"
-            />
-          </div>
-        </div>
+
         <div className="home-frame2">
           <div className="home-adr-statistics">
             <div className="home-statistic3">
@@ -166,37 +114,95 @@ const Home = () => {
             </span>
           </div>
         </div>
-        <div className="home-frame1">
-          <div className="home-hero-image">
-            <img
-              alt="Gray2512"
-              src="/external/gray2512-91s-800w.png"
-              className="home-gray"
-            />
-            <img
-              alt="Blue2513"
-              src="/external/blue2513-8gd-800w.png"
-              className="home-blue"
-            />
-            <img
-              alt="Nurse2510"
-              src="/external/nurse2510-5u3o-400w.png"
-              className="home-nurse"
-            />
-          </div>
-          <div className="home-findout-button">
-            <span className="home-text42">
-              <span>Find out how</span>
-            </span>
-          </div>
-          <span className="home-text44">
-            <span>
-              We’re enabling worldwide access to personalized precision
-              medicine.
-            </span>
-          </span>
-        </div>
         
+        <div className="home-frame3">
+          <div className="home-text14">
+            <div className="home-requestdemo-button">
+              <span className="home-text15">
+                <span>Request a demo</span>
+              </span>
+            </div>
+            <div className="home-learnmore-button">
+              <span className="home-text17">
+                <span>Learn more</span>
+              </span>
+            </div>
+            <span className="home-text19">
+              <span>
+                Our point-of-care pharmacogenomic test device helps doctors
+                personalize patient treatment plans by identifying adverse drug
+                reactions in 20 minutes.
+              </span>
+            </span>
+            <span className="home-text21">ProbeiT</span>
+              <span className="home-text22">OneDrug</span>
+              
+          </div>
+          <div className="home-placeholder-image">
+            <img
+              alt="Rectangle3516"
+              src="/external/probitDevice.png"
+              className="home-rectangle"
+            />
+          </div>
+        </div>
+
+        <div className="home-frame4">
+          <div className="home-partners">
+            <span className="home-text12">
+              <span>Trusted by leading institutions worldwide</span>
+            </span>
+            <img
+              alt="GreySloan1063"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/1c8dc7fb-084c-4576-ac70-b5772815c26a?org_if_sml=15119&amp;force_format=original"
+              className="home-grey-sloan"
+            />
+            <img
+              alt="PrincetonPlainsboro1062"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/f2ed2315-43c9-45dd-9ba6-652d699892c3?org_if_sml=111680&amp;force_format=original"
+              className="home-princeton-plainsboro"
+            />
+            <img
+              alt="UHN1062"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/8947b1d9-4a27-422b-b9c9-a69a48e3233e?org_if_sml=13690&amp;force_format=original"
+              className="home-uhn"
+            />
+            <img
+              alt="UofT1062"
+              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/64b188d9-a472-4b5a-9158-d52f2ed82634/db610096-cc1a-4c16-a6a4-d7fe86039a7e?org_if_sml=17907&amp;force_format=original"
+              className="home-uof-t"
+            />
+          </div>
+        </div>
+
+        <div className="home-footer">
+          <span className="home-text10">
+            <span>© 2024 OneDrug Inc. All rights reserved.</span>
+          </span>
+          <div className="home-social-media">
+            <img
+              alt="iconmonstrtwitter316511"
+              src="/external/iconmonstrtwitter316511-hifv.svg"
+              className="home-iconmonstrtwitter31"
+            />
+            <img
+              alt="iconmonstrlinkedin316511"
+              src="/external/iconmonstrlinkedin316511-0rq8.svg"
+              className="home-iconmonstrlinkedin31"
+            />
+            <img
+              alt="iconmonstrinstagram1316511"
+              src="/external/iconmonstrinstagram1316511-30hf.svg"
+              className="home-iconmonstrinstagram131"
+            />
+            <img
+              alt="iconmonstrfacebook316511"
+              src="/external/iconmonstrfacebook316511-6s1s.svg"
+              className="home-iconmonstrfacebook31"
+            />
+          </div>
+        </div>
+
       </div>
     </div>
   )
